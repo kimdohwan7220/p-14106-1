1ffa4ade1a98e1b39eb47f077e8b4156ce36b545
kimdohwan7220 <dohwan8220@naver.com>
2025-12-11 15:54:06 +0900

    feat: 36-3. Error 객체 사용하지 않고 응답객체를 바로 사용

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -50,10 +50,14 @@ export default function Page() {
         title: titleInput.value,
         content: contentTextarea.value,
       }),
-    }).then((data) => {
-      alert(data.msg);
-      router.replace(`/posts/${data.data.id}`);
-    });
+    })
+      .then((data) => {
+        alert(data.msg);
+        router.replace(`/posts/${data.data.id}`);
+      })
+      .catch((error) => {
+        alert(`${error.resultCode} : ${error.msg}`);
+      });
   };
 
   return (
